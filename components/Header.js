482e2ffe8bbdeebb5c417c373d6d5c81b3316561
George Kolesnikov <gkolesnikov88@gmail.com>
2023-11-19 18:40:54 +0300

    Header. Made Center part

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 import {
+    HomeIcon,
+    UserGroupIcon,
+} from '@heroicons/react/24/solid'
+import {
+    FlagIcon,
+    PlayIcon,
     MagnifyingGlassIcon,
+    ShoppingCartIcon
 } from '@heroicons/react/24/outline'
+import HeadeIcon from './HeadeIcon'
 
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center
+        p-2 lg:px-5 shadow-md
+    '>
         {/* Left */}
         <div className='flex items-center'>
             <Image 
@@ -18,7 +28,7 @@ function Header() {
             <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                 <MagnifyingGlassIcon className='h-6 text-gray-600'/>
                 <input 
-                    className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' 
+                    className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' 
                     type="text" 
                     placeholder='Search Facebook'
                 />
@@ -26,6 +36,15 @@ function Header() {
         </div>
 
         {/* Center */}
+        <div className='flex justify-center flex-grow-1'>
+            <div className='flex space-x-6 md:space-x-2'>
+                <HeadeIcon active Icon={HomeIcon}/>
+                <HeadeIcon Icon={FlagIcon}/>
+                <HeadeIcon Icon={PlayIcon}/>
+                <HeadeIcon Icon={ShoppingCartIcon}/>
+                <HeadeIcon Icon={UserGroupIcon}/>
+            </div>
+        </div>
 
         {/* Right */}
 
